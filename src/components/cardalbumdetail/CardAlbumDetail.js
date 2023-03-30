b553aebe82b79a3f1354097f6a3cb5a70f48d0e2
Ruludex <gastonghj@hotmail.com.ar>
2023-03-30 00:44:19 -0300

    version 1.6

--- src/components/cardalbumdetail/CardAlbumDetail.js
+++ src/components/cardalbumdetail/CardAlbumDetail.js
@@ -1,31 +1,31 @@
 import React from "react";
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
-import { collection,query,getDocs,where,documentId } from 'firebase/firestore';
+import { collection, query, getDocs, where, documentId } from 'firebase/firestore';
 import { db } from "../../firebase/FirebaseConfig";
 import CartBuy from "../cartbuy/cartBuy";
 import "./Style.css"
 
 const CardAlbumDetail = () => {
   const { id } = useParams();
-  const [albumData,setAlbumData] = useState ([]);
-  useEffect( () => {
+  const [albumData, setAlbumData] = useState([]);
+  useEffect(() => {
     const getAlbums = async () => {
-        const q = query(collection(db,"music"),where(documentId(),"==",id));
-        const querySnapshot = await getDocs (q);
-        const docs = []
-        querySnapshot.forEach ((doc) => {
-            docs.push({...doc.data(),id:doc.id})
-        } );
-        setAlbumData(docs);
+      const q = query(collection(db, "music"), where(documentId(), "==", id));
+      const querySnapshot = await getDocs(q);
+      const docs = []
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id })
+      });
+      setAlbumData(docs);
     }
     getAlbums();
-} , [id] )
-  return(
+  }, [id])
+  return (
     <div className='flex-container'>
-    {albumData.map((album) => {
-        return <CartBuy key={album.id} Album={album}/>   
-    })}
+      {albumData.map((album) => {
+        return <CartBuy key={album.id} Album={album} />
+      })}
     </div>
   )
 }
